f7f1d7de67473c255a515477307b69494358e7be
Martin Studer <martin@fluxlabs.ch>
2023-05-22 23:05:18 +0200

    updated plugin version

--- plugin.php
+++ plugin.php
@@ -2,8 +2,8 @@
 require_once __DIR__ . "/vendor/autoload.php";
 
 $id = "iqse";
-$version = "0.1.1";
-$ilias_min_version = "5.3.0";
+$version = "2023.05.22";
+$ilias_min_version = "7.0";
 $ilias_max_version = "7.999";
 $responsible = "fluxlabs AG";
 $responsible_mail = "support@fluxlabs.ch";
